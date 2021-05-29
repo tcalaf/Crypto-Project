42f7cfbcd4caf430849d74df8fd46b0e6fa004e5
Tudor <tudorcalafeteanu@hotmail.com>
2021-05-29 14:44:40 +0300

    Added name, symbol, standard for the token and implemented balanceOf and transfer function

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,14 +3,31 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Token {
 	uint256 public totalSupply;
+	string public name = "LabraCoin";
+	string public symbol = "LABRA";
+	string public standard = "LabraCoin v1.0";
+
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+	);
+
+	mapping(address => uint256) public balanceOf;
 
 	// Constructor
-	constructor () public{
-		totalSupply = 1000000;
-
+	constructor (uint256 _initialSupply) public {
+		balanceOf[msg.sender] = _initialSupply;
+		totalSupply = _initialSupply;
 	}
 
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		require(balanceOf[msg.sender] >= _value);
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+		
+		emit Transfer(msg.sender, _to, _value);
 
-	// Set the total number of tokens
-	// Read the total number of tokens
+		return true;
+	}
 }
